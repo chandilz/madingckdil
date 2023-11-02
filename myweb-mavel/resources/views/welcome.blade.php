cf74057f9a53436f65af5053bdcc9156d4690248
chandafa <130040496+chandafa@users.noreply.github.com>
2023-11-02 10:37:40 +0700

    Update welcome.blade.php

--- myweb-mavel/resources/views/welcome.blade.php
+++ myweb-mavel/resources/views/welcome.blade.php
@@ -5,6 +5,9 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;600&display=swap" rel="stylesheet">
     <link href="{{ asset('css/style.css') }}" rel="stylesheet">
     <title>Madingku</title>
 </head>
@@ -42,7 +45,7 @@
     <div class="footer_fixed">
     <!-- Running Text -->
 <div class="kotak">
-        
+
 
     <div class="footer">
         <div class="jam-body" id="jam">
