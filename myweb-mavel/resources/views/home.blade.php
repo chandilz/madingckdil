e0be7e406198efc9988b3cc1bf860b72260809db
chandafa <130040496+chandafa@users.noreply.github.com>
2023-11-03 00:52:28 +0700

    Update home.blade.php

--- myweb-mavel/resources/views/home.blade.php
+++ myweb-mavel/resources/views/home.blade.php
@@ -8,6 +8,39 @@
 
 @section('content')
     <p>Welcome to this beautiful admin panel.</p>
+    <div class="container_dashboard">
+
+        {{-- user_register --}}
+        <div class="small-box bg-gradient-success">
+            <div class="inner">
+              <h3>44</h3>
+              <p>User Registrations</p>
+            </div>
+            <div class="icon">
+              <i class="fas fa-user-plus"></i>
+            </div>
+            <a href="#" class="small-box-footer">
+              More info <i class="fas fa-arrow-circle-right"></i>
+            </a>
+          </div>
+
+          {{-- user_admin --}}
+          <div class="small-box bg-info">
+            <div class="inner">
+              <h3>150</h3>
+              <p>New Orders</p>
+            </div>
+            <div class="icon">
+              <i class="fas fa-shopping-cart"></i>
+            </div>
+            <a href="#" class="small-box-footer">
+              More info <i class="fas fa-arrow-circle-right"></i>
+            </a>
+          </div>
+
+
+    </div>
+
 @stop
 
 @section('css')
