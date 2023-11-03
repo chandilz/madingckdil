b30a7a38790d5a9c458f4704d28681d2e0d43cae
aaidilz <aidil.233040059@mail.unpas.ac.id>
2023-11-03 10:40:24 +0700

    add admin setting view

---
+++ myweb-mavel/resources/views/settings/mading.blade.php
@@ -0,0 +1,5 @@
+@extends('adminlte::page')
+@section('title', 'Dashboard')
+@section('content_header')
+    <h1>SETTING MADING</h1>
+@stop
---
+++ myweb-mavel/resources/views/settings/video.blade.php
@@ -0,0 +1,5 @@
+@extends('adminlte::page')
+@section('title', 'Dashboard')
+@section('content_header')
+    <h1>SETTING VIDEO</h1>
+@stop
